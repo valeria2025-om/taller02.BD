90271df6b260053fc77b8279e9e6269d9508f214
valeria <valeriaorrego1143257@correo.itm.edu.co>
2025-09-24 19:52:37 -0500

    script

--- README.md
+++
@@ -1,1 +0,0 @@
-# taller02.BD
---
+++ script.sql
@@ -0,0 +1,43 @@
+SELECT COUNT(DISTINCT cc.IdCancion) AS TotalCanciones
+FROM Compositor c
+JOIN Cancion_Compositor cc ON c.Id = cc.IdCompositor
+WHERE CHARINDEX('JUANES', c.Nombre) > 0;
+
+SELECT i.Id, r.Ritmo
+FROM Cancion c
+JOIN Interpretacion i ON c.Id = i.IdCancion
+JOIN Ritmo r ON i.IdRitmo = r.Id
+WHERE c.Titulo = 'Lluvia'
+
+SELECT DISTINCT c.Titulo
+FROM Cancion c
+JOIN Cancion_Compositor cc ON c.Id = cc.IdCancion
+JOIN Compositor comp ON cc.IdCompositor = comp.Id
+JOIN Interpretacion i ON c.Id = i.IdCancion
+JOIN Interprete intr ON i.IdInterprete = intr.Id
+JOIN Ritmo r ON i.IdRitmo = r.Id
+WHERE r.Ritmo = 'Balada'
+  AND CHARINDEX(intr.Nombre, comp.Nombre) > 0;
+ 
+SELECT DISTINCT p.Nombre
+FROM Interprete intr
+JOIN Tipo t ON intr.IdTipoInterprete = t.Id
+JOIN Pais p ON intr.IdPais = p.Id
+JOIN Interpretacion i ON intr.Id = i.IdInterprete
+JOIN Ritmo r ON i.IdRitmo = r.Id
+WHERE r.Ritmo = 'Salsa' AND t.Tipo = 'Grupo';
+
+SELECT DISTINCT c.Titulo, intr.Nombre AS Interprete
+FROM Cancion c
+JOIN Interpretacion i ON c.Id = i.IdCancion
+JOIN Interprete intr ON i.IdInterprete = intr.Id
+WHERE c.Titulo IN ('Candilejas', 'Malagueña');
+
+SELECT intr.Nombre,
+       COUNT(DISTINCT cc.IdCancion) AS CancionesCompuestas,
+       COUNT(DISTINCT i.IdCancion) AS CancionesInterpretadas
+FROM Interprete intr
+JOIN Compositor comp ON CHARINDEX(intr.Nombre, comp.Nombre) > 0
+LEFT JOIN Cancion_Compositor cc ON comp.Id = cc.IdCompositor
+LEFT JOIN Interpretacion i ON intr.Id = i.IdInterprete
+GROUP BY intr.Nombre;
